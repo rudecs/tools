2da57a37923f4683570995fd0bf0e0b9451e6b2a
vadim.sorokin <vadim.sorokin@digitalenergy.online>
2018-10-23 13:49:22 +0300

    THRESHOLD from env var

--- debugging/linuxki.sh
+++ debugging/linuxki.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-THRESHOLD="1"
+if [ -z "$THRESHOLD" ]; then
+    THRESHOLD="20"
+fi
 SLEEP="10"
 WORKDIR="/opt/de/linuxki"
 export PATH=$PATH:/opt/linuxki
@@ -20,9 +22,12 @@ while :; do
     if [ -f $WORKDIR/stop ]; then
         break
     fi
+    echo $THRESHOLD
     LA=`cat /proc/loadavg|cut -d "." -f 1`
     if [ "${LA}" -ge "${THRESHOLD}" ]; then
+	
 	date > $WORKDIR/stop
+	echo "THRESHOLD: $THRESHOLD" >> $WORKDIR/stop
         runki -f
     fi
     sleep $SLEEP
