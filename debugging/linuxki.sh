3f227c73d0d256a9fe95f42a94a5fee999e34db6
vadim.sorokin <vadim.sorokin@digitalenergy.online>
2018-10-24 15:30:32 +0300

    3 перехода через порог

--- debugging/linuxki.sh
+++ debugging/linuxki.sh
@@ -5,7 +5,18 @@ if [ -z "$THRESHOLD" ]; then
 fi
 SLEEP="10"
 WORKDIR="/opt/de/linuxki"
+STOPFILE="$WORKDIR/stop_`date +%d-%m-%Y_%H-%M-%S`"
 export PATH=$PATH:/opt/linuxki
+FLAG="0x0"
+I=0
+
+function runrunki() {
+#    echo "$STOPFILE"
+    date > $STOPFILE
+    echo "THRESHOLD: $THRESHOLD" >> $STOPFILE
+    runki -f
+}
+
 
 #Create workdir
 if [ ! -d $WORKDIR ]; then
@@ -19,16 +30,23 @@ fi
 
 # Start daemon
 while :; do
-    if [ -f $WORKDIR/stop ]; then
+    if [ $I -gt 2 ]; then
+#    if [ -f $WORKDIR/stop ]; then
         break
     fi
-    echo $THRESHOLD
     LA=`cat /proc/loadavg|cut -d "." -f 1`
+    echo "LA: $LA"
+    echo "FLAG: $FLAG"
+    echo "THRESHOLD: $THRESHOLD"
     if [ "${LA}" -ge "${THRESHOLD}" ]; then
-	
-	date > $WORKDIR/stop
-	echo "THRESHOLD: $THRESHOLD" >> $WORKDIR/stop
-        runki -f
+	echo "first if"
+	if [ "$FLAG" == "0x0" ]; then
+            echo "second if"
+            I=$(($I+1))
+	    runrunki
+	fi
+	FLAG="0x1"
+    else FLAG="0x0"; echo "else"
     fi
     sleep $SLEEP
 done
